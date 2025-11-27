5b72f3c335693e0b4313a6fa1434ce579718052a
marco-rinaldi <iczqmmm71956@outlook.com>
2025-11-27 14:59:09 +0800

    deploy

--- deploy/deploy.ts
+++ deploy/deploy.ts
@@ -5,13 +5,14 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployer } = await hre.getNamedAccounts();
   const { deploy } = hre.deployments;
 
-  const deployedFHECounter = await deploy("FHECounter", {
+  const mintSphere = await deploy("MintSphereNFT", {
     from: deployer,
     log: true,
+    args: ["MintSphere", "SPHERE", "https://mint-sphere.example/api/token/"],
   });
 
-  console.log(`FHECounter contract: `, deployedFHECounter.address);
+  console.log(`MintSphereNFT contract: ${mintSphere.address}`);
 };
 export default func;
-func.id = "deploy_fheCounter"; // id required to prevent reexecution
-func.tags = ["FHECounter"];
+func.id = "deploy_mintSphere"; // id required to prevent reexecution
+func.tags = ["MintSphereNFT"];
